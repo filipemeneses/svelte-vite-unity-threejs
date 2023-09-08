abf07d2094273551e6ae0bf442d6919d9daa8426
Filipe <filipemeneses@users.noreply.github.com>
2023-09-07 21:47:46 -0300

    feat: use unity-to-json watcher

--- src/vite/unityThreejs.ts
+++ src/vite/unityThreejs.ts
@@ -1,19 +1,13 @@
 import fs from 'fs/promises';
 import path from 'path';
-import watch from 'glob-watcher';
 import {
-  convertUnityProjectToJson,
   interpretScene,
+  createUnityProjectToJsonWatcher,
 } from 'unity-to-json';
-import { makeQueue } from './makeQueue';
 
 const UNITY_PROJECT_PATH = path.resolve(__dirname, '../../unity/UnityProject');
 const SCENE_FILENAME = 'SampleScene.unity';
 const OUTPUT_PATH = '../lib/unity-scene.json';
-
-let sceneFiles: any;
-let guidMapping: any;
-let filenameMapping: any;
 
 const writeScene = async (json) => {
   await fs.writeFile(
@@ -22,56 +16,15 @@ const writeScene = async (json) => {
   );
 };
 
-const main = async () => {
-  (
-    {
-      sceneFiles,
-      guidMapping,
-      filenameMapping,
-    } = await convertUnityProjectToJson({
-      unityProjectRootFolderPath: UNITY_PROJECT_PATH,
-    })
-  );
-
-  const scene = sceneFiles.find((r) => r.filename.endsWith(SCENE_FILENAME));
-
-  await writeScene(
-    await interpretScene({
-      sceneData: scene?.data,
-      guidMapping,
-      filenameMapping,
-    }),
-  );
-};
-
-const start = async () => {
-  await main();
-
-  const relativeWindowsSafeUrl = (url) => path.relative(
-    process.cwd(),
-    path.resolve(__dirname, url),
-  ).replace(/\\/g, '/');
-
-  const SCENE_FILES_GLOB = relativeWindowsSafeUrl(`${UNITY_PROJECT_PATH}/**/Assets/Scenes/*.unity`);
-  const META_FILES_GLOB = relativeWindowsSafeUrl(`${UNITY_PROJECT_PATH}/**/Assets/**/*.meta`);
-  const FBX_FILES_GLOB = relativeWindowsSafeUrl(`${UNITY_PROJECT_PATH}/**/Assets/**/*.fbx`);
-
-  const watcher = watch([
-    SCENE_FILES_GLOB,
-    META_FILES_GLOB,
-    FBX_FILES_GLOB,
-  ], {
-    delay: 0,
-  });
-
-  const queue = makeQueue();
-
-  watcher.on('change', async (filepath: string) => {
-    if (filepath.endsWith('.meta')) {
-      return;
-    }
-
-    queue.add(main);
+const startWatcher = async () => {
+  createUnityProjectToJsonWatcher({
+    unityProjectRootFolderPath: UNITY_PROJECT_PATH,
+    sceneFilename: SCENE_FILENAME,
+    async onSceneChange(context) {
+      await writeScene(
+        await interpretScene(context),
+      );
+    },
   });
 };
 
@@ -79,7 +32,7 @@ const start = async () => {
 const unitySyncPlugin = {
   name: 'unity-sync-plugin',
   async config() {
-    await start();
+    await startWatcher();
   },
 };
 
